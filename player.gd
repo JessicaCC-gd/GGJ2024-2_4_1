0cd12ebd35aeb8af58aabc566b814b203e50f09e
Luis Reis <luiseduardo.mreis@gmail.com>
2024-01-26 22:16:31 +0000

    Player animation

--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var vel : Vector2 = Vector2()
 var moveSpeed: float = 2.0 # n meters per second
@@ -8,18 +10,24 @@ var moveSpeed: float = 2.0 # n meters per second
 func _physics_process(delta):
 	var input = Vector2()
 	if Input.is_action_pressed("move_up") :
-		$AnimationPlayer.
-		input.y -= 
+		_animated_sprite.play("walk")
+		input.y -= 1
 		input.x = 0
 	elif Input.is_action_pressed("move_down"):
+		_animated_sprite.play("walk")
 		input.y += 1
 		input.x = 0
 	elif Input.is_action_pressed("move_left"):
+		_animated_sprite.play("walk")
 		input.x -= 1
 		input.y = 0
 	elif Input.is_action_pressed("move_right"):
+		_animated_sprite.play("walk")
 		input.x += 1
 		input.y = 0
+	else:
+		_animated_sprite.play("idle")
+
 	var dir = (transform.y * input.y + transform.x * input.x)
 	vel.x = dir.x * moveSpeed
 	vel.y = dir.y * moveSpeed
