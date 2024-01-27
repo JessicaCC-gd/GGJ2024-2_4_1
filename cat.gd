242f29f2bc3c2543faa651d9e3bd3ed64bd353ab
Luis Reis <luiseduardo.mreis@gmail.com>
2024-01-27 15:05:56 +0000

    Cat despawn

--- cat.gd
+++ cat.gd
@@ -2,5 +2,20 @@ extends Node2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
+var smoke_scene = preload("res://smoke.tscn")
+var despawn_timer
+
 func _ready():
 	_animated_sprite.play("default")
+	despawn_timer = Timer.new()
+	add_child(despawn_timer)
+	despawn_timer.wait_time = 2.0
+	despawn_timer.timeout.connect(_despawn)
+	despawn_timer.start()
+	
+func _despawn():
+	print("despawn cat")
+	var smoke = smoke_scene.instantiate()
+	smoke.position = position
+	get_parent().add_child(smoke)
+	queue_free()
--- smoke.gd
+++ smoke.gd
@@ -1,4 +1,9 @@
 extends Node2D
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
+func _ready():
+	_animated_sprite.play("default")
+
 func _on_animated_sprite_2d_animation_finished():
 	get_node(".").queue_free()
