0da72f18b86cc098523b47155a8ea527b3dc3b83
luis <luis@kynd.io>
2024-01-27 09:54:08 +0000

    Add initial enemy movement stuff

--- enemy.gd
+++ enemy.gd
@@ -5,8 +5,18 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var _animated_sprite = $AnimatedSprite2D
 
-func _physics_process(delta):
+@onready var _nav_agent = $NavigationAgent2D
+@onready var _animated_sprite = $AnimatedSprite2D
+func _ready():
+	_nav_agent.set_target_position(get_node("../Player").position)
 	
+		
+func _physics_process(delta):
 	_animated_sprite.play("idle")
+	if not $NavigationAgent2D.is_navigation_finished():
+		var movement_delta = SPEED * delta
+		var current_agent_position = global_position
+		var next_path_position = _nav_agent.get_next_path_position()
+		velocity = (next_path_position - current_agent_position).normalized() * movement_delta
+		#move_and_slide()
--- player.gd
+++ player.gd
@@ -7,7 +7,7 @@ var speed = 200
 func _physics_process(delta):
 	var input_dir = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
-		input_dir.x -= 1
+		input_dir.x -= 1	
 	if Input.is_action_pressed("move_right"):
 		input_dir.x += 1
 	if Input.is_action_pressed("move_up"):
