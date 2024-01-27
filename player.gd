b4375a884448c25dee2c8cce325fa37aafa3993a
jess <jessicacarrico.gd@gmail.com>
2024-01-27 09:07:00 +0000

    player movement animation fixed

--- player.gd
+++ player.gd
@@ -7,24 +7,37 @@ var speed = 200
 func _physics_process(delta):
 	var input_dir = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
-		_animated_sprite.play("walk")
-		_animated_sprite.rotation_degrees = 270
 		input_dir.x -= 1
 	if Input.is_action_pressed("move_right"):
-		_animated_sprite.play("walk")
-		_animated_sprite.rotation_degrees = 90
 		input_dir.x += 1
 	if Input.is_action_pressed("move_up"):
-		_animated_sprite.play("walk")
-		_animated_sprite.rotation_degrees = 0
 		input_dir.y -= 1
 	if Input.is_action_pressed("move_down"):
-		_animated_sprite.play("walk")
-		_animated_sprite.rotation_degrees = 180
 		input_dir.y += 1
 
+	player_animation(input_dir)
 	velocity = input_dir.normalized() * speed
-	if input_dir == Vector2(0,0):
-		_animated_sprite.play("idle")
-
 	move_and_collide(velocity * delta)
+
+
+func player_animation(input_dir):
+	if input_dir.x != 0 || input_dir.y != 0:
+		_animated_sprite.play("walk")
+	else:
+		_animated_sprite.play("idle")
+	if input_dir.x > 0 && input_dir.y == 0:
+		_animated_sprite.rotation_degrees = 90
+	elif input_dir.x > 0 && input_dir.y > 0:
+		_animated_sprite.rotation_degrees = 135
+	elif input_dir.x > 0 && input_dir.y < 0:
+		_animated_sprite.rotation_degrees = 45
+	elif input_dir.x < 0 && input_dir.y == 0:
+		_animated_sprite.rotation_degrees = -90
+	elif input_dir.x < 0 && input_dir.y > 0:
+		_animated_sprite.rotation_degrees = -135
+	elif input_dir.x < 0 && input_dir.y < 0:
+		_animated_sprite.rotation_degrees = -45
+	elif input_dir.x == 0 && input_dir.y < 0:
+		_animated_sprite.rotation_degrees = 0
+	elif input_dir.x == 0 && input_dir.y > 0:
+		_animated_sprite.rotation_degrees = 180
