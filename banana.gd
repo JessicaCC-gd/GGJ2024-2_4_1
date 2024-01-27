4eb6f940566ae1fdf29789dd990293d0fd75faca
luis <luis@kynd.io>
2024-01-27 19:04:17 +0000

    add banana despawn

--- banana.gd
+++ banana.gd
@@ -8,11 +8,11 @@ var despawn_timer
 
 func _ready():
 	_animated_sprite.play("default")
-	#despawn_timer = Timer.new()
-	#add_child(despawn_timer)
-	#despawn_timer.wait_time = 2.0
-	#despawn_timer.timeout.connect(_despawn)
-	#despawn_timer.start()
+	despawn_timer = Timer.new()
+	add_child(despawn_timer)
+	despawn_timer.wait_time = 2.0
+	despawn_timer.timeout.connect(_despawn)
+	despawn_timer.start()
 	
 func _despawn():
 	var smoke = smoke_scene.instantiate()
