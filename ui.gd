0cad2d4ce4d00ed3d1c3cbf2296e31354cfe41da
luis <luis@kynd.io>
2024-01-27 21:41:27 +0000

    add emotion respoonsive

--- ui.gd
+++ ui.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
 @onready var player = $"../TileMap/Player"
+@onready var _laugh_meter = $laughter_meter
+@onready var emotion = $Emotion
 
 func _ready():
 	pass # Replace with function body.
@@ -12,3 +14,14 @@ func _process(delta):
 	$label_2.set_text(str(player.no_tea))
 	$label_4.set_text(str(floor(player.dash_cooldown_timer.time_left)))
 	$label_3.set_text(str(player.no_banana))
+	
+	if _laugh_meter.value < 20:
+		emotion.frame = 0
+	if _laugh_meter.value > 40:
+		emotion.frame = 1
+	if _laugh_meter.value >60:
+		emotion.frame = 2
+	if _laugh_meter.value > 80:
+		emotion.frame = 3
+	if _laugh_meter.value == 100:
+		emotion.frame = 4
